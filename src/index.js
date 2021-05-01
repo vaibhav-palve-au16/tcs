cd15329ada26dfbeb2bed0b8ced615ba7d3c970e
vaibhav <vaibhavpalve1234@gmail.com>
2021-05-01 15:18:15 +0530

    done

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+const path1 = path.join(__dirname , '../public')
+app.use(express.static(path1 ))
+console.log(path1)
+
+app.get('/',(req,res)=>{
+    res.send('<h1>welcome to my home page</h1>');
+})
+
+app.get("/about",(req,res)=>{
+res.send('welcome to my about page');
+});
+
+app.get("/resume.pdf",(req,res)=>{
+    res.send('<a href = "../resume.pdf"  type="_blank">  r </a>')
+});
+
+app.get("/temp",(req,res)=>{
+res.send('welcome to my temp page');
+})
+ 
+app.listen(process.env.PORT || 3000)
